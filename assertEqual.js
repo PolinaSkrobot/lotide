5f222b675c11c887d320043486556f0eb2d486c1
Polina Skrobot <chi.polly.no@gmail.com>
2021-08-16 23:09:06 +0000

    Mu own asserEqual

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  const emo1 = String.fromCodePoint(0x1F6D1);
+  const emo2 = String.fromCodePoint(0x2705);
+  if (actual === expected) {
+    console.log(`${emo2}${emo2}${emo2} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${emo1}${emo1}${emo1} Assertion Failed: ${actual} !== ${expected}`);
+  }
+    
+};
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(5, 1);
