a6a7a711111ba2cfcf587787a09fdb18ad336906
Polina Skrobot <chi.polly.no@gmail.com>
2021-08-17 23:54:19 +0000

    Emojis changed

--- head.js
+++ head.js
@@ -1,20 +1,14 @@
-// const assertEqual = function(actual, expected) {
-//   const emo1 = String.fromCodePoint(0x1F6D1);
-//   const emo2 = String.fromCodePoint(0x2705);
-//   if (actual === expected) {
-//     console.log(`${emo2}${emo2}${emo2} Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`${emo1}${emo1}${emo1} Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
-// const head = function(someArray) {
-//   return someArray[0];
-// };
-// assertEqual(head([3]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-x = 12;
-if(true) {
-  x + 2;
-  x = 4;
-}
-console.log(x);
+const assertEqual = function(actual, expected) {
+  const emo1 = String.fromCodePoint(0x1F6D1);
+  const emo2 = String.fromCodePoint(0x2705);
+  if (actual === expected) {
+    console.log(`${emo2}${emo2}${emo2} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${emo1}${emo1}${emo1} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const head = function(someArray) {
+  return someArray[0];
+};
+assertEqual(head([3]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
