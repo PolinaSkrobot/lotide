14c488accbbf95afddc127a8c45dda26f610a9e7
Polina Skrobot <chi.polly.no@gmail.com>
2021-08-18 02:54:39 +0000

    modified

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,34 @@
-const assertArraysEqual = function(arr1,arr2) {
-  if (arr1.length !== arr2.length) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-    return;
-  }
+// const assertArraysEqual = function(arr1,arr2) {
+//   if (arr1.length !== arr2.length) {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+//     return;
+//   }
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+//       return;
+//     }
+//   }
+//   console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+//   return;
+// };
+const emo1 = String.fromCodePoint(0x1F6D1);
+const emo2 = String.fromCodePoint(0x2705);
+const eqArrays = function(arr1,arr2) {
+  let equal = true;
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-      return;
+      equal = false;
     }
   }
-  console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  return;
+  return equal;
+};
+const assertArraysEqual=function (arr1,arr2) {
+  if (eqArrays(arr1,arr2)) {
+    console.log(`${emo2}${emo2}${emo2} Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`${emo1}${emo1}${emo1} Assertion Failed: ${arr1} !== ${arr2}`);
+  }
 };
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
