0569ae9d2876052b24def6aea7f607196b41674b
Polina Skrobot <chi.polly.no@gmail.com>
2021-08-18 22:41:34 +0000

    LettersPosition.js created

--- letterPositions.js
+++ letterPositions.js
@@ -1,6 +1,6 @@
 const letterPositions = function(sentence) {
   const results = {};
-  const newSent = sentence;//.replace(/\s/g, '');
+  const newSent = sentence.toLowerCase();//.replace(/\s/g, '');
   for (let i = 0; i < newSent.length; i++) {
     if (newSent.charAt(i) === ' ') {
       i++;
