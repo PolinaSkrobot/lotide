d77c5eaccbcf54d1f7077d03d4cb78a51f7fa900
Polina Skrobot <chi.polly.no@gmail.com>
2021-08-23 20:36:20 +0000

    Tests are added

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,3 @@
-const assertEqual = function(actual, expected) {
-  const emo1 = String.fromCodePoint(0x1F6D1);
-  const emo2 = String.fromCodePoint(0x2705);
-  if (actual === expected) {
-    console.log(`${emo2}${emo2}${emo2} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${emo1}${emo1}${emo1} Assertion Failed: ${actual} !== ${expected}`);
-  }
-    
-};
 const eqArrays = function(arr1,arr2) {
   let equal = true;
   if (arr1.length !== arr2.length) {
@@ -22,16 +12,5 @@ const eqArrays = function(arr1,arr2) {
   }
   return equal;
 };
-// const eqArrays = function(x, y) {
-//   return Array.isArray(x) &&
-//     Array.isArray(y) &&
-//     x.length === y.length &&
-//     x.every((element, index) => element === y[index]);
-// };
 
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+module.exports = eqArrays;
