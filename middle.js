ab620fc484b268f1214484686fc98a432eca2ed9
Polina Skrobot <chi.polly.no@gmail.com>
2021-08-23 21:09:45 +0000

    Tests are added to Middle

--- middle.js
+++ middle.js
@@ -1,25 +1,4 @@
-const eqArrays = function(arr1,arr2) {
-  let equal = true;
-  if (arr1.length !== arr2.length) {
-    equal = false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        equal = false;
-        break;
-      }
-    }
-  }
-  return equal;
-};
 
-const assertArraysEqual = function(arr1,arr2) {
-  if (eqArrays(arr1,arr2)) {
-    console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
-  }
-};
 const middle = function(array) {
   let midd = Math.floor(array.length / 2);
   if (array.length <= 2) {
@@ -30,11 +9,5 @@ const middle = function(array) {
     return [array[midd]];
   }
 };
-console.log(middle([9,8,9,0]));
-console.log(middle([5,9,0]));
-console.log(middle([9,0]));
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6,7,8]), [4,5]);
-assertArraysEqual(middle([1, 5]), []);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6,7,8,9,10,11,12,13]), [7]);
+
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,8 @@
+const assertArraysEqual = require("../assertArraysEqual");
+const middle = require("../middle")
+
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6,7,8]), [4,5]);
+assertArraysEqual(middle([1, 5]), []);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6,7,8,9,10,11,12,13]), [7]);
