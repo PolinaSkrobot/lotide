32f5c34fbc667cab697a999589a4bc62a5795a16
Polina Skrobot <chi.polly.no@gmail.com>
2021-08-24 02:31:21 +0000

    Index created

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const countLetters = require('./countLetters');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const countOnly = require('./countOnly');
+const eqObjects = require('./eqObjects');
 const eqArrays = require('./eqArrays');
-const assertArraysEqual = require('./assertArraysEqual');
-const assertEqual = require('./assertEqual');
 
 module.exports = {
   head,
   tail,
   middle,
-  eqArrays,
-  assertArraysEqual,
-  assertEqual
+  countLetters,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  countOnly,
+  eqObjects,
+  eqArrays
+  
 };
